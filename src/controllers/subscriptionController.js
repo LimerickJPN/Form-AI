7f1b2c401e38173a79680339ba7b5261d795895a
LimerickJPN <ryo_takahashi@limerick.co.jp>
2025-05-31 23:53:18 +0900

    Delete src directory

--- src/controllers/subscriptionController.js
+++
@@ -1,7 +0,0 @@
-exports.subscribe = (req, res) => {
-    res.send('Subscription activated (not implemented yet)');
-};
-
-exports.unsubscribe = (req, res) => {
-    res.send('Subscription cancelled (not implemented yet)');
-};
